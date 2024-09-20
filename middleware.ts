90387d9e89199246bba9d2d6467c5bf27600cf0a
Usenobong Etokakpan <jabulanietokakpan@gmail.com>
2024-09-20 13:29:35 +0100

    Fixing middleware

--- middleware.ts
+++ middleware.ts
@@ -6,7 +6,7 @@ export default clerkMiddleware(( auth, req ) => {
 
 const isProtectedRoute = createRouteMatcher([
     '/dashboard(.*)',
-    '/'
+    // '/'
 ])
 export const config = {
   matcher: [
