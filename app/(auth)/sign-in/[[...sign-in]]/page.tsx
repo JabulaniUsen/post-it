46ebf3bc3b50a6c329f153ab5887fd4283137070
Usenobong Etokakpan <jabulanietokakpan@gmail.com>
2024-09-20 04:34:40 +0100

    ggg

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,9 +1,15 @@
+import Footer from "@/components/landingPage/Footer";
+import Navbar from "@/components/landingPage/Navbar";
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
     return (
-        <div className="flex item-center justify-center h-screen mt-20">
-            <SignIn />
+        <div className="">
+            <Navbar />
+            <div className="flex item-center justify-center h-screen mt-20">
+                <SignIn />
+            </div>
+            <Footer/>
         </div>
     )
 }
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,10 +1,16 @@
+import Footer from "@/components/landingPage/Footer";
+import Navbar from "@/components/landingPage/Navbar";
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
     return (
-        <div className="flex item-center justify-center h-screen mt-20">
-            <SignUp />
-        </div>  
+        <div>
+            <Navbar />
+            <div className="flex item-center justify-center h-screen mt-20">
+                <SignUp />
+            </div>  
+            <Footer/>
+        </div>
     )
 
 }
